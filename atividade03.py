d9a5c996dbbacb3e5678c89e9b163ffa072bc1e8
SarahRodrigues20 <113391554+SarahRodrigues20@users.noreply.github.com>
2022-09-30 07:59:57 -0300

    Add files via upload

---
+++ atividade03.py
@@ -0,0 +1,13 @@
+num1=int(input("Digite um número: "))
+num2=int(input("Digite outro número: "))
+
+if num1 > num2:
+    print(f"O maior número é: {num1}")
+else:
+    print(f"O maior numéro é: {num2}")
+
+
+
+
+
+    
